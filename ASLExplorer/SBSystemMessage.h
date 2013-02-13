f1d89ee4ecc60f1b4b4334a6893f87dc70511023
Paul Dardeau <pauldardeau@me.com>
2013-02-13 14:37:13 -0600

    added README

---
+++ ASLExplorer/SBSystemMessage.h
@@ -0,0 +1,13 @@
+//
+//  SBSystemMessage.h
+//  ASLExplorer
+//
+//  Created by Paul Dardeau on 2/8/13.
+//  Copyright (c) 2013 Paul Dardeau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBSystemMessage : NSObject
+
+@end
